8623539d9417874b380264117c5f4e9c309283d0
rupakpoddar <poddarrupak2808@gmail.com>
2024-06-24 15:35:00 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Mac app to test trackpad haptic feedback
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:macos_haptic_feedback/macos_haptic_feedback.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _macosHapticFeedback = MacosHapticFeedback();
+  bool triggerHaptics = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: const Color.fromRGBO(0, 123, 255, 1.0),
+      ),
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: const Text('MacBook Trackpad Haptic Feedback Test'),
+        // ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ...[
+                (
+                function: _macosHapticFeedback.generic,
+                text: '10 ms',
+                delay: 10,
+                ),
+                (
+                function: _macosHapticFeedback.alignment,
+                text: '25 ms',
+                delay: 25,
+                ),
+                (
+                function: _macosHapticFeedback.levelChange,
+                text: '50 ms',
+                delay: 50,
+                )
+              ].map((e) => Container(
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(0, 123, 255, 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
+                ),
+                margin: const EdgeInsets.all(10),
+                width: 200,
+                height: 200,
+                child: MouseRegion(
+                  onEnter: (event) async {
+                    if (!triggerHaptics) {
+                      triggerHaptics = true;
+                    }
+                    while (triggerHaptics) {
+                      e.function();
+                      await Future.delayed(Duration(milliseconds: e.delay));
+                    }
+                  },
+                  onExit: (event) {
+                    if (triggerHaptics) {
+                      triggerHaptics = false;
+                    }
+                  },
+                  child: Center(
+                    child: Text(
+                      e.text,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 21,
+                      ),
+                    )
+                  ),
+                ),
+              )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
